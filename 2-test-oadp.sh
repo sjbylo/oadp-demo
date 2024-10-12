70e54733d6cce5e13bb80b31002836bb2ebf22e6
Stephen Bylo <StephenBylo@gmail.com>
2024-10-12 14:08:58 +0800

    testing and fixes

--- 2-test-oadp.sh
+++ 2-test-oadp.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+echo_green()	{ [ "$TERM" ] && tput setaf 2; echo -e "$@"; [ "$TERM" ] && tput sgr0; }
 
 n=rhel9-$RANDOM
 
@@ -12,29 +14,35 @@ sleep 2
 
 time oc wait --for=jsonpath='{.status.phase}'=Running vmi/$n --timeout=40s -n demo-oadp
 sleep 1
+echo_green VM runnning
 
 # Waiting for agent to be connected
 oc -n demo-oadp wait --for=jsonpath='{.status.conditions[?(@.type=="AgentConnected")].status}'=True vmi/$n --timeout=90s
+echo_green VM agent up
 
 oc get vmi $n -n demo-oadp
 
 # backup
-sed "s/rhel9-1/$n/g" backup.yaml 
-sed "s/rhel9-1/$n/g" backup.yaml | oc apply -f - -n openshift-adp
+sed -i "s/rhel9-.*/$n/g" backup.yaml 
+cat backup.yaml | oc apply -f - -n openshift-adp
 sleep 2
 time oc -n openshift-adp wait --for=jsonpath='{.status.phase}'=Completed backup.velero.io/backup-$n --timeout=40s
+echo_green Backup complete
 sleep 2
 
 # Delete
 oc delete vm $n -n demo-oadp
+echo_green VM deleted
 sleep 1
 
 # restore
-sed "s/rhel9-1/$n/g" restore.yaml 
-sed "s/rhel9-1/$n/g" restore.yaml | oc apply -f - -n openshift-adp
+sed -i "s/rhel9-.*/$n/g" restore.yaml 
+cat restore.yaml | oc apply -f - -n openshift-adp
 sleep 1
 time oc -n openshift-adp wait --for=jsonpath='{.status.phase}'=Completed restore.velero.io/restore-$n --timeout=40s
+echo_green Restore complete
 sleep 2
 ##virtctl start $n
 oc get vmi $n -n demo-oadp
+echo_green VM scheduled
 
